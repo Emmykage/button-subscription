4a2ee47d817562b192e1be1ee22137497cb5e66a
morris menanya <emmiemenz@gmail.com>
2023-07-20 16:41:51 +0100

    initialize

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
-import Image from 'next/image'
+'use client'
+import React, { useState } from 'react';
+import { Button, Modal } from "antd"
+import { CheckCircleOutlined, CheckOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import {BsShieldExclamation} from "react-icons/bs"
+import {MdOutlineWorkspacePremium} from "react-icons/md"
+import {BsRocketTakeoff} from "react-icons/bs"
 
-export default function Home() {
+const Home: React.FC = () => {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+       <div className="m-auto">
+        <Button className='bg-blue-300' type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      {/* <Modal className='w-full' title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>np
+        <p>Some contents...</p>
+      </Modal> */}
+        
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+       </div>
+       <div className={isModalOpen ? 'absolute w-full min-h-screen' : 'absolute w-full min-h-screen hidden'}>
+        <div className=' text-end p-5'><CloseCircleOutlined className='w-8 text-gray-600 text-4xl' onClick={handleCancel} /></div>
+        <div className='flex justify-center min-h-screen absolute w-full'>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      
+       <div className='grid grid-cols-3 gap-10 max-w-4xl m-auto' >
+          <div className='bg-white hover:shadow-lg p-6 rounded-md border border-orange-500 transition ease-out duration-500'>
+            <h2 className='font-bold text-2xl my-4 flex'><BsShieldExclamation  className='mr-2 w-8 h-8 text-orange-400 border-4 rounded-full p-1 border-orange-400'/> 
+          <span>Basic</span>
+ 
+            </h2>
+            <ul>
+              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-orange-100 text-orange-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover basic services. get started with basic package</span></li>
+              <li className='my-5 items-center'><CheckOutlined className='bg-orange-100 text-orange-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>250 Leads</span></li>
+              <li className='my-5 flex items-center'><CheckOutlined className='bg-orange-100 text-orange-700 mr-4 rounded-full p-1' /><span className='text-sm '>Monthly: $190/month ($0.76 per lead)</span> </li>
+            </ul>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            <span className='block text-center font-bold text-2xl text-orange-500'>$2280</span>
+            <p className='text-center text-sm my-3'>Annually: Save 15%</p>
+            
+            <Button className='bg-orange-100 text-orange-600 font-bold w-full hover:border hover:border-orange-400 hover:bg-white'  onClick={showModal}> Purchase  </Button>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                        
+          </div>
+          <div className='bg-white hover:shadow-md p-6 transition ease-out duration-500 rounded-md border  border-purple-500'>
+            <h2 className='font-bold text-2xl my-4 flex'><BsRocketTakeoff  className='mr-2 w-8 h-8 text-purple-400 border-4 rounded-full p-1 border-purple-400'/> 
+          <span>Advanced</span>
+ 
+            </h2>
+            <ul>
+              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover basic services. get started with basic package</span></li>
+              <li className='my-5 items-center'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>250 Leads</span></li>
+              <li className='my-5 flex items-center'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /><span className='text-sm '>Monthly: $190/month ($0.76 per lead)</span> </li>
+            </ul>
+
+            <span className='block text-center font-bold text-2xl text-purple-500'>$2280</span>
+            <p className='text-center text-sm my-3'>Annually: Save 15%</p>
+            
+            <Button className='bg-purple-100 text-purple-600 font-bold w-full hover:text-white hover:bg-white' onClick={showModal}> Purchase  </Button>
+
+                        
+          </div>
+          <div className='bg-white hover:shadow-md transition ease-out duration-500 p-6 rounded-md border border-teal-500'>
+            <h2 className='font-bold text-2xl my-4 flex'><MdOutlineWorkspacePremium  className='mr-2 w-8 h-8 text-teal-400 border-4 rounded-full p-1 border-teal-400'/> 
+          <span>Premium</span>
+ 
+            </h2>
+            <ul>
+              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover basic services. get started with basic package</span></li>
+              <li className='my-5 items-center'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>250 Leads</span></li>
+              <li className='my-5 flex items-center'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /><span className='text-sm '>Monthly: $190/month ($0.76 per lead)</span> </li>
+            </ul>
+
+            <span className='block text-center font-bold text-2xl text-teal-500'>$2280</span>
+            <p className='text-center text-sm my-3'>Annually: Save 15%</p>
+            
+            <Button className='bg-teal-100 text-teal-600 font-bold w-full'  onClick={showModal}> Purchase  </Button>
+
+                        
+          </div>
+          
+       </div>
+       </div>
+       </div>
     </main>
   )
 }
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        lemon: '#2596be'
+
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
