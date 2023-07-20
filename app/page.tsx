215891114eaa78b1ef866153da564d816de1a22d
morris menanya <emmiemenz@gmail.com>
2023-07-20 18:58:04 +0100

    add transition effect

--- app/page.tsx
+++ app/page.tsx
@@ -13,36 +13,40 @@ const Home: React.FC = () => {
     setIsModalOpen(true);
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+  let displayMode = {opacity: 0, pointerEvents: "none" }
+  if(isModalOpen){
+    displayMode.opacity = 1,
+    displayMode.pointerEvents = "auto"
+
+  }else{
+    displayMode.opacity = 0,
+    displayMode.pointerEvents = "none"
+
+
+  }
+  console.log(isModalOpen)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
        <div className="m-auto">
         <Button className='bg-blue-300' type="primary" onClick={showModal}>
-        Open Modal
+          open Modal
       </Button>
-      {/* <Modal className='w-full' title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>np
-        <p>Some contents...</p>
-      </Modal> */}
-        
-
        </div>
-       <div className={isModalOpen ? 'absolute w-full min-h-screen' : 'absolute w-full min-h-screen hidden'}>
-        <div className=' text-end p-5'><CloseCircleOutlined className='w-8 text-gray-600 text-4xl' onClick={handleCancel} /></div>
+       <div 
+       style={displayMode}
+       
+       className={ 'absolute w-full min-h-screen transition ease-linear duration-300'}>
+        <div className=' text-end p-5 absolute w-full top-0 z-10 '><CloseCircleOutlined className='w-8 text-gray-600 text-4xl' onClick={handleCancel} /></div>
         <div className='flex justify-center min-h-screen absolute w-full'>
 
         
 
       
        <div className='grid grid-cols-3 gap-10 max-w-4xl m-auto' >
-          <div className='bg-white hover:shadow-lg p-6 rounded-md border border-orange-500 transition ease-out duration-500'>
+          <div className='bg-white hover:shadow-lg p-6 rounded-lg border border-orange-500 transition ease-out duration-500'>
             <h2 className='font-bold text-2xl my-4 flex'><BsShieldExclamation  className='mr-2 w-8 h-8 text-orange-400 border-4 rounded-full p-1 border-orange-400'/> 
           <span>Basic</span>
  
@@ -60,36 +64,36 @@ const Home: React.FC = () => {
 
                         
           </div>
-          <div className='bg-white hover:shadow-md p-6 transition ease-out duration-500 rounded-md border  border-purple-500'>
+          <div className='bg-white hover:shadow-md p-6 transition ease-out duration-500 rounded-lg border  border-purple-500'>
             <h2 className='font-bold text-2xl my-4 flex'><BsRocketTakeoff  className='mr-2 w-8 h-8 text-purple-400 border-4 rounded-full p-1 border-purple-400'/> 
           <span>Advanced</span>
  
             </h2>
             <ul>
-              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover basic services. get started with basic package</span></li>
-              <li className='my-5 items-center'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>250 Leads</span></li>
+              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover upgraded services. get started with basic package</span></li>
+              <li className='my-5 items-center'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>500 Leads</span></li>
               <li className='my-5 flex items-center'><CheckOutlined className='bg-purple-100 text-purple-700 mr-4 rounded-full p-1' /><span className='text-sm '>Monthly: $190/month ($0.76 per lead)</span> </li>
             </ul>
 
-            <span className='block text-center font-bold text-2xl text-purple-500'>$2280</span>
+            <span className='block text-center font-bold text-2xl text-purple-500'>$3780</span>
             <p className='text-center text-sm my-3'>Annually: Save 15%</p>
             
             <Button className='bg-purple-100 text-purple-600 font-bold w-full hover:text-white hover:bg-white' onClick={showModal}> Purchase  </Button>
 
                         
           </div>
-          <div className='bg-white hover:shadow-md transition ease-out duration-500 p-6 rounded-md border border-teal-500'>
+          <div className='bg-white hover:shadow-md transition ease-out duration-500 p-6 rounded-lg border border-teal-500'>
             <h2 className='font-bold text-2xl my-4 flex'><MdOutlineWorkspacePremium  className='mr-2 w-8 h-8 text-teal-400 border-4 rounded-full p-1 border-teal-400'/> 
           <span>Premium</span>
  
             </h2>
             <ul>
-              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover basic services. get started with basic package</span></li>
-              <li className='my-5 items-center'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>250 Leads</span></li>
+              <li className='my-5 flex items-center text-gray-700 text-sm'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /> <span className='font-semibold my-5' >Ideal to Discover Premium services. get started with basic package</span></li>
+              <li className='my-5 items-center'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /><span className='text-sm font-medium my-5'>1000 Leads</span></li>
               <li className='my-5 flex items-center'><CheckOutlined className='bg-teal-100 text-teal-700 mr-4 rounded-full p-1' /><span className='text-sm '>Monthly: $190/month ($0.76 per lead)</span> </li>
             </ul>
 
-            <span className='block text-center font-bold text-2xl text-teal-500'>$2280</span>
+            <span className='block text-center font-bold text-2xl text-teal-500'>$6630</span>
             <p className='text-center text-sm my-3'>Annually: Save 15%</p>
             
             <Button className='bg-teal-100 text-teal-600 font-bold w-full'  onClick={showModal}> Purchase  </Button>
